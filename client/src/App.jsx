993a85dfd83dc81fc39d932ea0f8d0ec3783379f
Md Raza <mdraza8397@gmail.com>
2026-07-27 16:18:06 +0530

    frontend setup done

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+
+const App = () => {
+  return (
+    <div>
+      <Login />
+      <Register />
+    </div>
+  );
+};
+
+export default App;
